75b9db5e7c7af0f82caf30c1f9dee8aa6e84eb37
yoandria <yoandria@student.42antananarivo.mg>
2026-04-13 15:08:36 +0300

    Added standard headers, clause guards and the prototype of ft_printf in the header file ft_printf.h

---
+++ ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoandria <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 14:11:29 by yoandria          #+#    #+#             */
+/*   Updated: 2026/04/13 14:25:24 by yoandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+// Standard headers
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
+
+// Function prototypes
+int	ft_printf(const char *, ...);
+
+#endif
