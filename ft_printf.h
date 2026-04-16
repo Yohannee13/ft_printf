97cab20ebe1511a5f704f4bb1ad3e7120083ed0c
yoandria <yoandria@student.42antananarivo.mg>
2026-04-16 10:11:01 +0300

    Added the prototypes of the helper function in ft_printf.h and finished the main function in ft_printf.c

--- ft_printf.h
+++ ft_printf.h
@@ -1,24 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yoandria <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/13 14:11:29 by yoandria          #+#    #+#             */
-/*   Updated: 2026/04/13 14:25:24 by yoandria         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-// Standard headers
-# include <stdlib.h>
 # include <stdarg.h>
 # include <unistd.h>
 
-// Function prototypes
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
+int	ft_print_char(int c);
+int	ft_print_str(char *s);
+int	ft_print_ptr(unsigned long long ptr);
+int	ft_print_nbr(int n);
+int	ft_print_unbr(unsigned int n);
+int	ft_print_hex(unsigned int n, const char format);
+int	ft_print_percent(void);
 
 #endif
